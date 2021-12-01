78fffbb99096fa2c7ee026f9b6a306476a5af898
wym97147 <91337417+wym97147@users.noreply.github.com>
2021-12-01 16:29:23 +0000

    step activity.R
    In order to be able to know the time required for students to do a question, use the data in step activity to calculate the time difference, and calculate the longest and shortest time for students to do the question.

--- src/enrolments.R
+++ src/enrolments.R
@@ -1,7 +1,5 @@
 #enrolment,删除存在unknow的行
 library(dplyr)
-##delete all the row with NA
-enrolments=na.omit(enrolments)
 gender = enrolments1 %>% filter(gender != "Unknown")
 gender=table(gender$gender)
 gender=sort(gender,decreasing = T)[1:2]
@@ -11,7 +9,6 @@ class(age_range)
 age_range=table(age_range$age_range)
 age_range=sort(age_range,decreasing = T)[1:7]
 age_range=as.data.frame(age_range)
-country = cyber.security.1_enrolments %>% filter(country != "Unknown")
 highest_education = enrolments1 %>% filter(highest_education_level != "Unknown")
 highest_education=table(highest_education$highest_education_level)
 highest_education=sort(highest_education,decreasing = T)
@@ -24,23 +21,19 @@ employment_area=table(employment_area$employment_area)
 employment_area=sort(employment_area,decreasing = T)
 employment_area=as.data.frame(employment_area)
 detected_country = enrolments1 %>% filter(detected_country != "--")
-table_country=table(country$country)
-country_1=sort(table_country,decreasing = T)[1:10]
-class(country_1)
-country_1=as.data.frame(country_1)
 table_detected = table(detected_country$detected_country)
 detected_country1=sort(table_detected,decreasing=T)[1:10]
 class(detected_country1)
-
+detected_country1=as.data.frame(detected_country1)
 ##作图
 library(ggplot2)
 library(fmsb)
 #可视化country_1
-ggplot(country_1,aes(x=Var1,y=Freq,fill=country_1[,1],group=factor(1)))+geom_bar(stat="identity")
-ggplot(age_range,aes(x=Var1,y=Freq,fill=age_range[,1],group=factor(1)))+geom_point(stat="identity")
 ggplot(gender,aes(x=Var1,y=Freq,fill=gender[,1],group=factor(1)))+geom_bar(stat="identity")
+ggplot(age_range,aes(x=Var1,y=Freq,fill=age_range[,1],group=factor(1)))+geom_point(stat="identity")
 ggplot(highest_education,aes(x=Var1,y=Freq,fill=highest_education[,1],group=factor(1)))+geom_bar(stat="identity")
-ggplot(employment_area,aes(x=Var1,y=Freq,fill=employment_area[,1],group=factor(1)))+geom_point(stat="identity")
+ggplot(detected_country1,aes(x=Var1,y=Freq,fill=country_1[,1],group=factor(1)))+geom_bar(stat="identity")
+ggplot(employment_area,aes(x=Var1,y=Freq,fill=employment_area[,1],group=factor(1)))+geom_bar(stat="identity")
 #print radarchart of employment status
 radarfig<-rbind(rep(1300,8),rep(100,8),employment_status)
 radarfig=as.data.frame(radarfig)
--- src/step activity.R
+++ src/step activity.R
@@ -6,13 +6,31 @@ step_activity=rbind(cyber.security.1_step.activity,cyber.security.2_step.activit
                     cyber.security.7_step.activity)
 
 step_activity=step_activity %>% filter(last_completed_at != "")
+
+
+#1.1
+step_activity_1.1=step_activity %>% filter(step == "1.1")
+step_activity_1.1
+step_1.1=step_activity_1.1[,5:6]
+step_1.1
+h=as.data.frame(step_1.1)
+#计算时间差
+h1=difftime(as.POSIXct(ymd_hms(h$last_completed_at)),ymd_hms(h$first_visited_at),units='secs')
+class(h1)
+h1=as.numeric(h1)
+q=mean(h1)
+h2=as.data.frame(h1)
+view(h2)
+apply(h2,2,function(x){which.max(x)})
+(h2[58,])
+apply(h2,2,function(x){which.min(x)})
+(h2[4918,])
+#1.14
 step_activity_1.14=step_activity %>% filter(step == "1.14")
 step_activity_1.14
 step_1.14=step_activity_1.14[,5:6]
 step_1.14
 s=as.data.frame(step_1.14)
-(attributes(s))
-d=as.POSIXct(s)
 #计算时间差
 s1=difftime(as.POSIXct(ymd_hms(s$last_completed_at)),ymd_hms(s$first_visited_at),units='secs')
 class(s1)
@@ -22,20 +40,4 @@ apply(s2,2,function(x){which.max(x)})
 (s2[1036,])
 apply(s2,2,function(x){which.min(x)})
 (s2[508,])
-colnames(s1)=c("s1")
-ggplot(s2,aes(s1))+geom_freqpoly(bins=30)
-
-
-
-##1.1
-step_activity=step_activity %>% filter(last_completed_at != "")
-step_activity_1.14=step_activity %>% filter(step == "1.14")
-step_activity_1.14
-step_1.14=step_activity_1.14[,5:6]
-step_1.14
-s=as.data.frame(step_1.14)
-(attributes(s))
-d=as.POSIXct(s)
-#计算时间差
-s1=difftime(as.POSIXct(ymd_hms(s$last_completed_at)),ymd_hms(s$first_visited_at),units='secs')
 
