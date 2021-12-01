524bbdb9947d7c7733e9790f7c146ae7b258b34b
wym97147 <91337417+wym97147@users.noreply.github.com>
2021-12-01 23:07:16 +0000

    Update video.R
    Analyze the amount of video playback, draw a density map to check the average number of playbacks, and explore the relationship between total playback and total downloads, research on the form of video playback, and research on the proportion of different regions.

--- src/video.R
+++ src/video.R
@@ -5,28 +5,28 @@ video_stats=video_stats[,-19]
 video_stats=video_stats[,-25]
 video_stats=video_stats[,-2]
 video_stats1=as.data.frame(video_stats)
-view(video_stats1)
+dim(video_stats1)
+#可视化total view
+p1<-ggplot(video_stats1,aes(total_views))
+bp5<-p1+geom_density(color="red",fill="yellow")
+bp5
+#可视化total download
 p<-ggplot(video_stats1,aes(total_downloads))
 bp4<-p+geom_density(color="red",fill="gray")
 bp4
+#Explore the relationship between total view and total download 
 video_stats_1=video_stats1[1:13,]
 class(video_stats_1)
 library(reshape2)
 dd=video_stats_1[,1:4]
-p2<-ggplot(dd,aes(x=total_views,y=total_downloads,group=factor(1)))+geom_point(stat="identity",color=dd$step_position)
+p2<-ggplot(dd,aes(x=total_views,y=total_downloads,group=factor(1)))+geom_point(stat="identity",color=dd$step_position)+geom_text(aes(label=step_position,vjust=-0.8,hjust=0.5),show.legend = TRUE)
 p2
-dd2=video_stats_1[,9:14]
-dd2
-ggplot(dd2,aes(x=viewed_ten_percent,y=viewed_twentyfive_percent,group=factor(1)))+geom_boxplot()
-ggplot(dd2,aes(x=viewed_ninetyfive_percent,y=viewed_onehundred_percent,group=factor(1)))+geom_boxplot()
-#选取时间最长视频做分析
-video_2.4=video_stats %>% filter(step_position == "2.4")
-total_view=video_2.4[,4:7]
-total_view4=as.data.frame(total_view)
-view(total_view4)
-#可视化
-ggplot(total_view4,aes(y=total_downloads))+geom_density(color="green",fill="red")
-ggplot(total_view4,aes(x=total_caption_views))+geom_density(color="black",fill="blue")
-ggplot(total_view4,aes(x=otal_transcript_views))+geom_density(color="black",fill="gray")
-ggplot(total_view4,aes(x=viewed_hd))+geom_density(color="black",fill="gray")
+#对视频播放形式的研究
+ggplot(video_stats_1,aes(x=step_position,y=total_caption_views))+geom_area(fill="blue",alpha=2)
+ggplot(video_stats_1,aes(x=step_position,y=viewed_hd))+geom_area(fill="red",alpha=2)
+#对不同地区的观看人数做出箱线图
+ggplot(video_stats_1,aes(x=step_position,y=europe_views_percentage,group=factor(1)))+geom_boxplot()
+ggplot(video_stats_1,aes(x=step_position,y=asia_views_percentage,group=factor(1)))+geom_boxplot()
+ggplot(video_stats_1,aes(x=step_position,y=oceania_views_percentage,group=factor(1)))+geom_boxplot()
+
 
